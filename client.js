f1b98a78125225813a1723eb4cc7b5b7abe9fb1c
Gabe Barrera <gsbarrera1@gmail.com>
2019-04-22 03:13:39 -0700

    added comments

--- client.js
+++ client.js
@@ -1,21 +1,46 @@
-const inquire = require('inquirer');
-const dgram = require('dgram');
-const client = dgram.createSocket('udp4');
+/**
+ * --- CLIENT.JS ---
+ * Author: Gabriel Barrera
+ * Programming Assignment 2
+ * CS 576, Prof. Wang
+ * 
+ * A client program binded to port 5433 that posts 
+ * string data to a server for concatenation.
+ * 
+ * Instructions: 
+ * - Run client with 'node client.js'
+ * - Enter a string to be concatenated
+ * - Press 'ENTER/RETURN' key to send to server
+ * - Concatenated string is returned to command line
+ * - If program is stuck, use "Ctrl+C" to terminate program
+ */
+const inquire = require('inquirer');        // Read user input module
+const dgram = require('dgram');             // UDP module
+const client = dgram.createSocket('udp4');  // Create UDP socket
 
+// User input prompt
 var questions = [{
   type: 'input',
   name: 'input',
-  message: "What would you like to send?",
-}]
+  message: "Input string to be sent: ",
+}];
 
+// Prompt user for input string
 inquire.prompt(questions).then(answers => {
+  // Store user input in buffer to be sent to server
   const message = Buffer.from(`${answers['input']}`);
 
-  client.send(message, 5432, 'localhost');
-
-  client.on('message', (msg, info) => {
-    console.log(`receiver got: ${msg} from ${info.address}:${info.port}`);
+  // Send message to server
+  client.send(message, 5432, 'localhost', function(error){
+    if(error) client.close(); // Terminate on error
+    else console.log(`*** Sent "${message}" ***`);
   });
-})
 
-client.bind(5433);
+  // Accept incoming message from server
+  client.on('message', (msg, info) => {
+    console.log(`CLIENT received:\n  ${msg}\nSENDER: ${info.address}:${info.port}`);
+    client.close(); // End program when concatenation complete
+  });
+});
+
+client.bind(5433); // Set client port to 5433
--- server.js
+++ server.js
@@ -1,26 +1,45 @@
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+/**
+ * --- SERVER.JS ---
+ * Author: Gabriel Barrera
+ * Programming Assignment 2
+ * CS 576, Prof. Wang
+ * 
+ * A server program binded to port 5432 that  
+ * receives string data from a client program,
+ * concatenates the string with a funny
+ * message, then return string back to the
+ * client
+ * 
+ * Instructions: 
+ * - Run server with 'node server.js'
+ * - Shut down with 'Ctrl+C'
+ */
+const dgram = require('dgram');             // UDP Module
+const server = dgram.createSocket('udp4');  // UDP Socket
 
 server.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   server.close();
 });
 
+// Accept incoming messages from a client
 server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  console.log(`SERVER received: \n  ${msg}\nSENDER: ${rinfo.address}:${rinfo.port}`);
+
+  // Concatenate client string with funny message
   concatString = msg + " - Don't eat yellow snow";
+
+  // Return concatenated string back to client
   server.send(concatString,5433,'localhost',function(error){
-    if(error){
-      client.close();
-    }else{
-      console.log('Sending Data...');
-    }
+    if(error) client.close();
+    else console.log('*** Returned concatenated string ***');
   });
 });
 
+// Display server socket address and port
 server.on('listening', () => {
   const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
+  console.log(`SERVER listening ${address.address}:${address.port}`);
 });
 
-server.bind(5432);
+server.bind(5432); // Set server port to 5432
